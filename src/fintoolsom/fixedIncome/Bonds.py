d6f8d60dcd6a16446ae8ca3efa750a9e1abd5b4f
oliverm91 <oliver.mohr.b@gmail.com>
2022-04-06 15:44:23 -0400

    add docstrings

--- src/fintoolsom/fixedIncome/Bonds.py
+++ src/fintoolsom/fixedIncome/Bonds.py
@@ -1,5 +1,6 @@
 import numpy as np
 from datetime import date
+from fintoolsom.rates.Rates import RateConvention, Rate
 from mathsom import numerics, solvers
 from .. import rates
 from .. import dates
@@ -96,7 +97,18 @@ class Bond:
         pvs = self.flows_amount  * future_flows_mask / wealth_factors
         return pvs
     
-    def get_present_value(self, date: date, irr_value: float, rate_convention) -> float:
+    def get_present_value(self, date: date, irr_value: float, rate_convention: RateConvention) -> float:
+        '''
+        Returns the present value of the bond at a given date.
+        ----------
+            date (date): date at which the present value is calculated.
+            irr_value (float): irr value of the bond.
+            rate_convention (RateConvention): rate convention of the bond.
+        ----
+        Returns:
+        ----
+            present_value (float): present value of the bond at the given date.
+        '''
         pvs = self.get_flows_pv(date, irr_value, rate_convention)
         total_pv = sum(pvs)
         return total_pv
@@ -109,7 +121,19 @@ class Bond:
         pv = sum(pvs)
         return pv
     
-    def get_irr(self, date: date, present_value: float, irr_rate_convention) -> float:
+    def get_irr(self, date: date, present_value: float, irr_rate_convention: RateConvention) -> Rate:
+        '''
+        Calculates the internal rate of return of a bond for a given present value.
+        --------
+        Parameters:
+        ----
+            date (date): The date for which the IRR is calculated.
+            present_value (float): The present value of the bond.
+            irr_rate_convention (RateConvention): The convention for the IRR.
+        ----
+        Returns:
+        ----
+            irr (float): The internal rate of return of the bond.'''
         irr_initial_guess = self.accrue_rate.rate_value
         objective_value = present_value
         args = [date, irr_initial_guess, irr_rate_convention]
@@ -119,6 +143,18 @@ class Bond:
         return irr
     
     def get_par_value(self, date: date, decimals: int=8) -> float:
+        '''
+        Calculates the par value (as of notional + accrued interest at current coupon rate) of the bond.
+        ----------
+        Parameters:
+        ----
+            date(date): The date for which the par value is calculated.
+            decimals(int): The number of round the result.
+        ----
+        Returns:
+        ----
+            par_value(float): The par value of the bond.
+        '''
         current_coupon = self.coupons.get_current_coupon(date)
         par_value = current_coupon.residual + current_coupon.get_accrued_interest(date)
         return round(par_value, decimals)
@@ -132,6 +168,21 @@ class Bond:
         return price, par_value
     
     def get_duration(self, date: date, irr: rates.Rate, day_count_convention: dates.DayCountConvention=dates.DayCountConvention.Actual, time_fraction_base: int=365) -> float:
+        '''
+        Calculates the bond duration.
+        ----------
+        Parameters:
+        ----
+            date (date): The date for which the duration is calculated.
+            irr (Rate): The interest rate.
+            day_count_convention (DayCountConvention): Optional. The day count convention. Default is Actual.
+            time_fraction_base (int): The time fraction base. Default is 365.
+        ----
+        Returns:
+        ----
+            duration (float): The bond duration.
+        '''
+
         end_dates = self.coupons.get_end_dates()
         tenors = dates.get_time_fraction(date, end_dates, day_count_convention, time_fraction_base)
         pvs = self.get_flows_pv(date, irr.rate_value, irr.rate_convention)
@@ -140,6 +191,19 @@ class Bond:
         return duration
     
     def get_dv01_approx(self, date: date, irr: rates.Rate, fx=1.0) -> float:
+        '''
+        Calculate dv01 of the bond with approximation formula: - present_value * duration / 10.000
+        ----------
+        Parameters:
+        ----
+            date (date): The date for which the dv01 is calculated.
+            irr (Rate): The interest rate.
+            fx (float): Optional. The foreign exchange rate. Default is 1.
+        ----
+        Returns:
+        ----
+            dv01 (float): The dv01 of the bond.
+        '''
         dur = self.get_duration(date, irr)
         pv = self.get_present_value(date, irr.rate_value, irr.rate_convention) / 100
         dv01 = - pv * dur * fx / 10_000
@@ -147,6 +211,19 @@ class Bond:
         return dv01
     
     def get_dv01(self, date: date, irr: rates.Rate, fx=1.0) -> float:
+        '''
+        Calculate dv01 of the bond with numeric differentiation.
+        ----------
+        Parameters:
+        ----
+            date (date): The date for which the dv01 is calculated.
+            irr (Rate): The interest rate.
+            fx (float): Optional. The foreign exchange rate. Default is 1.
+        ----
+        Returns:
+        ----
+            dv01 (float): The dv01 of the bond.
+        '''
         irr_value = irr.rate_value
         irr_rate_convention = irr.rate_convention
         valuation_parameters = [date, irr_value, irr_rate_convention]
