84091f5edddc1ca4288468592e6d4e77319f8c5f
Oliver Mohr <oliver.mohr.b@gmail.com>
2022-04-09 21:26:33 -0400

    _

--- src/fintoolsom/rates/Rates.py
+++ src/fintoolsom/rates/Rates.py
@@ -74,7 +74,7 @@ def get_rate_from_wf(wf: Union[np.ndarray, float], start_date, end_date, rate_co
     elif ic == InterestConvention.Exponential:
         rate_value = np.log(wf) / time_fraction
         
-    if isinstance(start_date,  get_args(Union[Sequence, np.ndarray])):
+    if isinstance(time_fraction,  np.ndarray):
         return [Rate(rate_convention, rv) for rv in rate_value]    
     else:
         return Rate(rate_convention, rate_value)
