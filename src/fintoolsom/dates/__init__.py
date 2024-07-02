f2500a7552201c53c5f9a68c20b5b8b7848c303e
oliverm91 <oliver.mohr.b@gmail.com>
2024-07-02 11:58:51 -0400

    .

--- src/fintoolsom/dates/__init__.py
+++ src/fintoolsom/dates/__init__.py
@@ -1,4 +1,4 @@
-from .date_counts import get_day_count, get_time_fraction
+from .date_counts import get_day_count, get_time_fraction, DayCountConvention
 from .calendars import Calendar, get_cl_calendar, get_ny_calendar
 from .holidays import FridayEasterRule, MonthDayRule, ConsecutiveHolidaySandwichRule, LastWeekWeekdayRule, OrdinalWeekWeekdayRule
 from .adjustments import FollowingConvention, ModifiedFollowingConvention, PrecedingConvention, ModifiedPrecedingConvention
