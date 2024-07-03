abab6320edd3c27848df59901105ae89e690640e
oliverm91 <oliver.mohr.b@gmail.com>
2024-07-03 12:18:31 -0400

    .

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,15 +11,13 @@ def print_cl_holidays(year: int) -> list[date]:
     year_holidays = cl_calendar.get_holidays(year)
     return year_holidays
 
-st.title('Test fintoolsom GitHub Repo')
-
+st.title('Using fintoolsom')
 try:
     year = st.number_input('Ingrese año:', value=2024, min_value=1900, max_value=2500, step=1)
     if st.button('Calcular feriados chilenos'):
         holidays = print_cl_holidays(year)
         holidays_str = [h.strftime('%Y-%m-%d') for h in holidays]
         holidays_df = pd.DataFrame(holidays_str, columns=[f'Feriados {year}'])
-        holidays_df.reset_index(drop=True, inplace=True)
         st.table(holidays_df)
 except Exception as e:
     st.error(f'Error executing code: {e}.')
