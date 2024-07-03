1beee9d97a819355acc3fac36891b57aa972dadc
oliverm91 <oliver.mohr.b@gmail.com>
2024-07-03 11:31:35 -0400

    .

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,27 +1,20 @@
+from datetime import date
 import streamlit as st
 import subprocess
 import sys
 
-def install_package():
-    subprocess.run([sys.executable, '-m', 'pip', 'install', 'git+https://github.com/oliverm91/fintoolsom.git', '--upgrade'], check=True)
-
-def execute_code():
+def print_cl_holidays(year: int) -> list[date]:
     from fintoolsom.dates.calendars import get_cl_calendar
     cl_calendar = get_cl_calendar()
-    year = 2024
     year_holidays = cl_calendar.get_holidays(year)
     return year_holidays
 
 st.title('Test fintoolsom GitHub Repo')
 
-if st.button('Install Latest Version and Execute Code'):
-    with st.spinner('Installing package...'):
-        install_package()
-    st.success('Package installed successfully!')
-
-    st.write('Executing code...')
-    try:
-        holidays = execute_code()
-        st.write(f'Holidays in 2024: {holidays}')
-    except Exception as e:
-        st.error(f'Error executing code: {e}')
+st.write('Executing code...')
+try:
+    year = st.number_input('Ingrese año:', value=2024, min_value=1900, max_value=2500, step=1)
+    holidays = print_cl_holidays(year)
+    st.write(f'Chilean Holidays in 2024: {holidays}')
+except Exception as e:
+    st.error(f'Error executing code: {e}')
