ab58e27a96f0d80b3912dbbb855b981f6a63e5b3
Oliver Mohr <oliver.mohr.b@gmail.com>
2024-07-04 00:46:26 -0400

    .

--- streamlit/app.py
+++ streamlit/app.py
@@ -1,15 +1,1 @@
 import streamlit as st
-
-import pages.feriados
-
-st.sidebar.title("Navigation")
-
-if st.sidebar.radio("Go to", ["Welcome"]) == "Welcome":
-    st.title("Welcome to the Home Page")
-    st.write("This is the main page.")
-
-# Dates group
-with st.sidebar.expander("Dates"):
-    dates_page = st.radio("Select a page", ["Feriados"])
-    if dates_page == "Feriados":
-        pages.feriados.show_page()
