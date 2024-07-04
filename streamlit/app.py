34f2701e73139d678891d44e5dad0e9421a084cb
Oliver Mohr <oliver.mohr.b@gmail.com>
2024-07-03 23:06:39 -0400

    .

--- streamlit/app.py
+++ streamlit/app.py
@@ -2,24 +2,18 @@ from datetime import date
 import traceback
 
 import pandas as pd
-
 import streamlit as st
 
-
-def print_cl_holidays(year: int) -> list[date]:
-    from fintoolsom.dates.calendars import get_cl_calendar
-    cl_calendar = get_cl_calendar()
-    year_holidays = cl_calendar.get_holidays(year)
-    return year_holidays
+from fintoolsom.dates.calendars import get_cl_calendar
 
 st.title('Using fintoolsom')
 try:
     year = st.number_input('Ingrese año:', value=2024, min_value=1900, max_value=2500, step=1)
     if st.button('Calcular feriados chilenos'):
-        holidays = print_cl_holidays(year)
+        cl_calendar = get_cl_calendar()
+        holidays = cl_calendar.get_holidays(year)    
         holidays_str = [h.strftime('%Y-%m-%d') for h in holidays]
         holidays_df = pd.DataFrame(holidays_str, columns=[f'Feriados {year}'])
         st.table(holidays_df)
 except Exception as e:
-    st.error(f'Error executing code: {e}.')
-    st.error(f'{traceback.format_exc()}')
+    st.exception(f'{traceback.format_exc()}')
