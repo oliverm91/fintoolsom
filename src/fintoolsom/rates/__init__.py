34e4a56f1f738354a1202482591b4004ddb97339
oliverm91 <oliver.mohr.b@gmail.com>
2024-10-02 10:19:55 -0300

    Updated rates init

--- src/fintoolsom/rates/__init__.py
+++ src/fintoolsom/rates/__init__.py
@@ -1,3 +1,2 @@
 from .Rates import Rate, RateConvention, InterestConventionBase, LinearInterestConvention, CompoundedInterestConvention, ExponentialInterestConvention
 from .ZeroCouponCurve import ZeroCouponCurve, ZeroCouponCurvePoint
-from .NelsonSiegelSvensson import NelsonSiegelSvensson
